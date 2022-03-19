3a79627130c77fbab6c53fa83b33ebb35ba5c6bf
SOM-0x3B1 <somadagaszkar@gmail.com>
2022-03-19 18:50:09 +0100

    Update assembly info

--- Symbolus/Properties/AssemblyInfo.cs
+++ Symbolus/Properties/AssemblyInfo.cs
@@ -1,16 +1,17 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TCoP-Reconquer")]
+[assembly: AssemblyTitle("Reconquer")]
 [assembly: AssemblyDescription("The first part of the game series The Chronicles of Pusztaszentistvan.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Onekilobit Studios")]
 [assembly: AssemblyProduct("TCoP-Reconquer")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,3 +35,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
